9a5dfbe9a6fb67593c85cc072e9c6a30dc7e895f
oldme <tyyn1022@gmail.com>
2024-01-18 11:26:49 +0800

    up

--- main.go
+++ main.go
@@ -3,8 +3,6 @@ package main
 import (
 	_ "GoFrame-Helper-Test/internal/packed"
 
-	_ "GoFrame-Helper-Test/internal/logic/logic.go"
-
 	"github.com/gogf/gf/v2/os/gctx"
 
 	"GoFrame-Helper-Test/internal/cmd"
